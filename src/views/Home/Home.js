d1613150d6fa81fbab95069b60a4686378745201
skulsta <sti.sku@gmail.com>
2021-04-14 15:32:56 +0200

    Select a user

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Users from "../../functions/Users";
 
 const Home = () => {
+  const [user, setUser] = useState(null);
+
+  const selectUser = (user) => setUser(user);
+
   const users = new Users();
   return (
     <div>
       <nav className="flex justify-end m-4">
         {console.log(users.getAll())}
         {users.getAll().map((user) => (
-          <p className="mx-2">{user.fullName}</p>
+          <div onClick={() => selectUser(user)} key={user.id} className="mx-2">
+            {user.fullName}
+          </div>
         ))}
       </nav>
     </div>
