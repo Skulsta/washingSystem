81e6ecb82e672f66a46021edf37d9c55b6234aae
skulsta <sti.sku@gmail.com>
2021-04-14 17:01:39 +0200

    Initial frontend

--- src/functions/WashingSystem.js
+++ src/functions/WashingSystem.js
@@ -2,10 +2,22 @@ module.exports = class WashingSystem {
   constructor(numberOfMachines) {
     this.machines = [];
     this.bookings = [];
+    this.washPrograms = [
+      { program: "Kokvask", time: 90 },
+      { program: "Tøyvask", time: 60 },
+      { program: "Håndvask", time: 20 },
+    ];
     for (var i = 1; i <= numberOfMachines; i++) {
-      this.machines.push(i);
+      this.addMachine();
     }
   }
+
+  addMachine = () => {
+    const machine = {
+      id: this.machines.length + 1,
+    };
+    return this.machines.push(machine);
+  };
 
   getAllMachines = () => this.machines;
   getAllBookings = () => this.bookings;
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Users from "../../functions/Users";
+import MachineImage from "../../assets/images/washing-machine.jpg";
 
 const Home = () => {
   const [user, setUser] = useState(null);
@@ -8,7 +9,7 @@ const Home = () => {
 
   const users = new Users();
   return (
-    <div>
+    <div className="max-w-screen-2xl flex flex-col mx-auto">
       <nav className="flex justify-end m-4">
         {console.log(users.getAll())}
         {users.getAll().map((user) => (
@@ -17,6 +18,33 @@ const Home = () => {
           </div>
         ))}
       </nav>
+      <div className="mt-12 p-8 mx-auto border rounded leading-loose space-y-8">
+        <div>
+          <img className="w-40 mx-auto" src={MachineImage} alt="Profile" />
+        </div>
+        <div>
+          <h2 className="text-2xl text-center text-gray-800">
+            Reserver tidspunkt for vask
+          </h2>
+          <p className="text-lg text-center text-gray-600">Forklaring her</p>
+        </div>
+        <form action="submit" className="mx-auto">
+          <form className="flex flex-col space-y-4">
+            <label>
+              Tidspunkt:
+              <input
+                className="border-b text-center w-16 ml-4"
+                type="text"
+                placeholder="13:00"
+              />
+            </label>
+            <label>
+              Vaskeprogram:
+              <input className="border-b text-center w-16 ml-4" type="select" />
+            </label>
+          </form>
+        </form>
+      </div>
     </div>
   );
 };
